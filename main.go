924f82759960974408f16a4f83b70507b9d9add2
purnasavitri <gekina01@gmail.com>
2025-03-21 12:27:43 +0800

    Koneksi Database dengan GORM

--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+type User struct {
+	ID			uint		`gorm:"column:id;primaryKey"`
+	Name 		string		`gorm:"column:name"`
+	Email 		string 		`gorm:"column:email"`
+	Age			string		`gorm:"column:age"`
+	CreatedAt	time.Time	`gorm:"column:createdAt"`
+	UpdatedAt	time.Time	`gorm:"column:UpdatedAt"`
 }
+
+func (User) TableName() string {
+    return "user"
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	dsn := "root:@tcp(localhost:3306)/openapi?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("failed to connect database")
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var result User
+		db.First(&result)
+		response, _ := json.Marshal(result)
+		fmt.Fprintf(w, string(response))
+	})
 	http.ListenAndServe(":3000", nil)
 }
